1701ed9ec3a1c8421b214b83964d535fd7216bc6
Rowan Griffin <rgriffi5@jaguarlandrover.com>
2025-08-26 13:59:10 +0100

    Broke out Status and Logging Functions

---
+++ src/StatusAndLogging.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "pinouts.h"
+#include "globals.h"
+#include "Libraries/Logger.h"
+
+void setBMSstatus(int newStatus, const char *message = nullptr);
+
+void setBMSstatus(int newStatus, int newError = 0, const char *message = nullptr);
+
+const char *getBMSStatusString(int status);
+
+const char *getBMSErrorString(int error);
+
+void alarmupdate();
+
+void printbmsstat();
