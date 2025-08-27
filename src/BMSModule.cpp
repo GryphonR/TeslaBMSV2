ac82219c6af3d0bf4465698858b3587c7a1dff07
Rowan <rowan@echook.uk>
2025-08-27 14:46:22 +0100

    Commenting

--- src/BMSModule.cpp
+++ src/BMSModule.cpp
@@ -41,6 +41,13 @@ void BMSModule::clearModule()
 /*
   Reading the status of the board to identify any flags, will be more useful when implementing a sleep cycle
 */
+
+/**
+ * @brief Reads the status of the board to identify any flags, will be more useful when implementing a sleep cycle
+ *
+ * Reads the Alert Status, Fault Status, COV Faults, CUV Faults, and Balance Time from the module and stores them in the
+ * alerts, faults, COVFaults, CUVFaults, and balanceTime variables respectively.
+ */
 void BMSModule::readStatus()
 {
   uint8_t payload[3];
@@ -84,21 +91,41 @@ void BMSModule::readStatus()
   */
 }
 
+/**
+ * @brief Gets the faults from the BMS module.
+ *
+ * @return The faults from the BMS module.
+ */
 uint8_t BMSModule::getFaults()
 {
   return faults;
 }
 
+/**
+ * @brief Gets the alerts from the BMS module.
+ *
+ * @return The alerts from the BMS module.
+ */
 uint8_t BMSModule::getAlerts()
 {
   return alerts;
 }
 
+/**
+ * @brief Gets the cells which have exceeded the overvoltage threshold.
+ *
+ * @return The cells which have exceeded the overvoltage threshold.
+ */
 uint8_t BMSModule::getCOVCells()
 {
   return COVFaults;
 }
 
+/**
+ * @brief Gets the cells which have exceeded the undervoltage threshold.
+ *
+ * @return The cells which have exceeded the undervoltage threshold.
+ */
 uint8_t BMSModule::getCUVCells()
 {
   return CUVFaults;
@@ -125,6 +152,12 @@ uint8_t BMSModule::getCUVCells()
   Tset = 35 + (5 * (buff[9] >> 4));
   } */
 
+/**
+ * @brief Stop balancing on this module.
+ *
+ * This function sends a command to the module to stop balancing. It does
+ * not return any data.
+ */
 void BMSModule::stopBalance()
 {
   uint8_t buff[8];
@@ -136,6 +169,14 @@ void BMSModule::stopBalance()
   delay(2);
 }
 
+/**
+ * @brief Reads the voltage and temperature values from the module.
+ *
+ * This function reads the voltage and temperature values from the module and stores them in the appropriate variables.
+ * It also updates the highest and lowest values for the module voltage and each cell voltage.
+ *
+ * @return true if the data was read successfully, false otherwise
+ */
 bool BMSModule::readModuleValues()
 {
   uint8_t payload[4];
@@ -233,12 +274,23 @@ bool BMSModule::readModuleValues()
   return retVal;
 }
 
+/**
+ * @brief Retrieves the voltage of a specific cell in the module
+ *
+ * @param cell cell number, valid values are 0-5
+ * @return The voltage of the specified cell in Volts
+ */
 float BMSModule::getCellVoltage(int cell)
 {
   if (cell < 0 || cell > 5) return 0.0f;
   return cellVolt[cell];
 }
 
+/**
+ * @brief Gets the lowest cell voltage of the module
+ *
+ * @return The lowest cell voltage in Volts, ignoring any cell voltage that is less than IgnoreCell
+ */
 float BMSModule::getLowCellV()
 {
   float lowVal = 10.0f;
@@ -246,6 +298,11 @@ float BMSModule::getLowCellV()
   return lowVal;
 }
 
+/**
+ * @brief Gets the highest cell voltage of the module
+ *
+ * @return The highest cell voltage in Volts, ignoring any cell voltage that is greater than 4.5V
+ */
 float BMSModule::getHighCellV()
 {
   float hiVal = 0.0f;
@@ -253,10 +310,22 @@ float BMSModule::getHighCellV()
   return hiVal;
 }
 
+/**
+ * @brief Calculates the average cell voltage of the module
+ *
+ * This function loops over all 6 cell voltage values and averages the ones that
+ * are within the valid range (IgnoreCell to 60.0V). It keeps track of the number
+ * of valid cells and if the number of valid cells changes, it increments a miss
+ * counter. If the number of valid cells is the same for 3 consecutive reads, it
+ * resets the miss counter and sets the number of valid cells to the current value.
+ *
+ * @return The average cell voltage of the module in Volts
+ */
 float BMSModule::getAverageV()
 {
   int x = 0;
   float avgVal = 0.0f;
+  // TODO - Should this be hard coded to 6?
   for (int i = 0; i < 6; i++)
   {
     if (cellVolt[i] > IgnoreCell && cellVolt[i] < 60.0)
@@ -287,43 +356,92 @@ float BMSModule::getAverageV()
   return avgVal;
 }
 
+/**
+ * @brief Gets the number of cells in the module that are within the valid voltage range
+ *
+ * This function returns the number of cells in the module that are within the valid
+ * voltage range (i.e. above the IgnoreCell value and below 60.0V). This value is
+ * calculated and cached by the getAverageV() function.
+ *
+ * @return The number of cells in the module that are within the valid voltage range
+ */
 int BMSModule::getscells()
 {
   return scells;
 }
 
+/**
+ * @brief Returns the highest module voltage that has been seen
+ *
+ * @return The highest module voltage in Volts
+ */
 float BMSModule::getHighestModuleVolt()
 {
   return highestModuleVolt;
 }
 
+/**
+ * @brief Returns the lowest module voltage that has been seen
+ *
+ * @return The lowest module voltage in Volts
+ */
 float BMSModule::getLowestModuleVolt()
 {
   return lowestModuleVolt;
 }
 
+/**
+ * @brief Returns the highest cell voltage of the specified cell that has been seen
+ *
+ * @param cell cell number, valid values are 0-5
+ * @return The highest cell voltage of the specified cell in Volts
+ */
 float BMSModule::getHighestCellVolt(int cell)
 {
   if (cell < 0 || cell > 5) return 0.0f;
   return highestCellVolt[cell];
 }
 
+/**
+ * @brief Returns the lowest cell voltage of the specified cell that has been seen
+ *
+ * @param cell cell number, valid values are 0-5
+ * @return The lowest cell voltage of the specified cell in Volts
+ */
 float BMSModule::getLowestCellVolt(int cell)
 {
   if (cell < 0 || cell > 5) return 0.0f;
   return lowestCellVolt[cell];
 }
 
+/**
+ * @brief Returns the highest temperature reading from the module
+ *
+ * @return The highest temperature in degrees Celsius
+ */
 float BMSModule::getHighestTemp()
 {
   return highestTemperature;
 }
 
+/**
+ * @brief Returns the lowest temperature reading from the module
+ *
+ * @return The lowest temperature in degrees Celsius
+ */
 float BMSModule::getLowestTemp()
 {
   return lowestTemperature;
 }
 
+/**
+ * @brief Returns the lowest temperature of the module
+ *
+ * If sensor is 0, returns the lowest of the two temperature readings. If sensor is 1 or 2, returns the
+ * temperature reading from the specified sensor.
+ *
+ * @return The lowest temperature in degrees Celsius
+ */
 float BMSModule::getLowTemp()
 {
   if (sensor == 0)
@@ -336,6 +454,14 @@ float BMSModule::getLowTemp()
   }
 }
 
+/**
+ * @brief Returns the highest temperature reading from the module
+ *
+ * If sensor is 0, returns the highest of the two temperature readings. If sensor is 1 or 2, returns the
+ * temperature reading from the specified sensor.
+ *
+ * @return The highest temperature in degrees Celsius
+ */
 float BMSModule::getHighTemp()
 {
   if (sensor == 0)
@@ -349,6 +475,14 @@ float BMSModule::getHighTemp()
 
 }
 
+/**
+ * @brief Calculates the average temperature of the module
+ *
+ * If sensor is 0, calculates the average of the two temperature readings. If sensor is 1 or 2, returns the
+ * temperature reading from the specified sensor.
+ *
+ * @return The average temperature in degrees Celsius
+ */
 float BMSModule::getAvgTemp()
 {
   if (sensor == 0)
@@ -361,43 +495,89 @@ float BMSModule::getAvgTemp()
   }
 }
 
+/**
+ * @brief Retrieves the module voltage of the module
+ *
+ * @return The module voltage in Volts
+ */
 float BMSModule::getModuleVoltage()
 {
   return moduleVolt;
 }
 
+/**
+ * @brief Retrieves the temperature of a specific sensor in the module
+ *
+ * @param temp Temperature sensor ID. Valid values are 0 or 1.
+ * @return The temperature in degrees Celsius
+ */
 float BMSModule::getTemperature(int temp)
 {
   if (temp < 0 || temp > 1) return 0.0f;
   return temperatures[temp];
 }
 
+/**
+ * @brief Sets the address of the module
+ *
+ * @param newAddr The new address of the module. Valid values are 1-62.
+ */
 void BMSModule::setAddress(int newAddr)
 {
   if (newAddr < 0 || newAddr > MAX_MODULE_ADDR) return;
   moduleAddress = newAddr;
 }
 
+/**
+ * @brief Gets the address of the module
+ *
+ * @return The address of the module (1-62)
+ */
 int BMSModule::getAddress()
 {
   return moduleAddress;
 }
 
+/**
+ * @brief Determines if the module is existing (i.e. if it was found during the findBoards() process)
+ *
+ * @return true if the module exists, false otherwise
+ */
 bool BMSModule::isExisting()
 {
   return exists;
 }
 
+/**
+ * @brief Sets the temperature sensor to read from the module.
+ *
+ * @param tempsensor The number of the temperature sensor to read from (0 or 1)
+ */
 void BMSModule::settempsensor(int tempsensor)
 {
   sensor = tempsensor;
 }
 
+/**
+ * @brief Sets the existence of the module
+ *
+ * @param ex true if the module exists, false otherwise
+ */
 void BMSModule::setExists(bool ex)
 {
   exists = ex;
 }
 
+/**
+ * @brief Sets the voltage level below which a cell is ignored during balancing.
+ *
+ * This is useful for cells that are not connected to the BMS (e.g. if you have
+ * a 12S BMS and you are using 10S or 14S batteries). You can set the ignore
+ * voltage to a value that is below the lowest cell voltage of the unused
+ * cells, so that the BMS does not try to balance the unused cells.
+ *
+ * @param Ignore The voltage level (in Volts) below which the cell is ignored.
+ */
 void BMSModule::setIgnoreCell(float Ignore)
 {
   IgnoreCell = Ignore;
